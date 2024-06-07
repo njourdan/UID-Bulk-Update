ab2365506d8a7e69b3dc2c286a801bf5363bfaf3
njourdan <njourdan@sigmaequipment.com>
2024-06-07 15:30:10 -0500

    minor tweaks

--- csvProcesses.js
+++ csvProcesses.js
@@ -29,20 +29,38 @@ export const parseAttributeMapping = async (filePath) => {
         });
     });
   };
-export const parseUIDList = (filePath) => {
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-    const records = parse(fileContent, {
-      columns: true,
-      skip_empty_lines: true
-    });
-    return records.map(item => item.UID);;
+  export const parseUIDList = (filePath) => {
+    // Read the file content
+    let fileContent = fs.readFileSync(filePath, 'utf8');
+    
+    // Split the file content into lines
+    const lines = fileContent.split('\n');
+  
+    // Check if there are more than 74 lines and remove the first 74 lines
+    if (lines.length > 74) {
+      const updatedContent = lines.slice(74).join('\n');
+  
+      // Write the updated content back to the file
+      fs.writeFileSync(filePath, updatedContent, 'utf8');
+  
+      // Parse the updated content
+      const records = parse(updatedContent, {
+        columns: true,
+        skip_empty_lines: true
+      });
+  
+      return records.map(item => item.UID);
+    } else {
+      // If there are fewer than 74 lines, return an empty array
+      return [];
+    }
   };
-export const addToFailList = createObjectCsvWriter({
-  path: './CSVs/fails.csv',
-  header: [
-    {id: 'UID', title: 'UID'},
-    {id: 'Reason', title: 'Reason'}
-
-  ],
-  append: fs.existsSync('./CSVs/fails.csv')
-})
+  
+  export const addToFailList = createObjectCsvWriter({
+    path: './CSVs/fails.csv',
+    header: [
+      { id: 'UID', title: 'UID' },
+      { id: 'Reason', title: 'Reason' }
+    ],
+    append: fs.existsSync('./CSVs/fails.csv')
+  });
--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import _ from 'lodash'
 import { razorCall } from "./apicalls.js";
 import { parseAttributeMapping, parseUIDList, addToFailList } from "./csvProcesses.js";
-async function run() {
+
 const attributeMappingPromise = parseAttributeMapping("./CSVs/Attributemapping.csv");
 const uidListPromise = parseUIDList('./CSVs/UIDlist.csv');
 const [attributeMapping, uidList] = await Promise.all([attributeMappingPromise, uidListPromise]);
 for (const uid of uidList) {
+    console.log(uid)
     let req = await razorCall(`/Asset/${uid}`,{})
     let uidInfo =await req.json()
     if(attributeMapping.hasOwnProperty(uidInfo.attributeSet)){
@@ -17,14 +18,13 @@ for (const uid of uidList) {
             method: "PUT",
             headers: {
                 'Accept': 'application/json',
-                'Content-Type': 'application/text',
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify({uidInfo})
 
         })
-        console.log(await resp)
-        // console.log(await resp.json())
-        if(await !resp.ok){
+        console.log(await resp.json())
+        if(!resp.ok){
             let data = [{ UID: uid, Reason: `ERROR: ${resp.status}, ${resp.statusText}` }]
             addToFailList.writeRecords(data)
             .then(() => {
@@ -37,8 +37,9 @@ for (const uid of uidList) {
 
     }else{
         console.log("Doesn't includes")
+        addToFailList.writeRecords([{ UID: uid, Reason: `ERROR: not in Mapping` }])
         // this asset isn't log it in the error output
 
     }
 
-  }}
+  }
